1f597874f239d84e604bdc4c579122582a1ce1a7
Ricard Boix <rboixp@gmail.com>
2022-10-17 16:57:14 +0200

    subida

--- Servidor/rws-chef-de-luxe/src/main/java/com/chefdeluxe/app/entidades/UsuariosRoles.java
+++ Servidor/rws-chef-de-luxe/src/main/java/com/chefdeluxe/app/entidades/UsuariosRoles.java
@@ -7,7 +7,8 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table(name = "usuarios_roles", uniqueConstraints = { @UniqueConstraint(columnNames = { "usuario_id" ,"rol_id"  })})
+@Table(name = "usuarios_roles", uniqueConstraints = { @UniqueConstraint(columnNames = { "usuario_id" }),
+		@UniqueConstraint(columnNames = { "rol_id" }) })
 public class UsuariosRoles {
 	@Id
 	private Long usuario_id;  
